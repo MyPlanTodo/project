939809fff585f33cf523545f188016422bb69cfa
Administrateur <administrateur@pc-ssi-1.(none)>
2013-01-23 15:11:10 +0100

    test.js

--- extension/chrome/content/test.js
+++ extension/chrome/content/test.js
@@ -1,10 +1,15 @@
 window.addEventListener("load", function load(event){
 	window.removeEventListener("load",load,false);
 	if(gBrowser){
-		gBrowser.addEventListener("pageshow",function plop(aEvent){
-			alert(document.title);
+		gBrowser.addEventListener("DOMContentLoaded",function plop(aEvent){
+			var doc = aEvent.originalTarget;
+			var win = doc.defaultView;
+			if ((doc.nodeName == '#document') && 
+			       (doc.defaultView.location.href == gBrowser.currentURI.spec)) 
+		        {
+				alert("page is loaded\n" + doc.location.href);
+			}
 		},false);
 	}
-});
-/.
+},false);
 function pop(){ alert('hello');} 
