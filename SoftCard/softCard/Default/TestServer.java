77da01d6ed35f53df601a5aac606ce935ab16227
Maguy <dingowow@free.fr>
2013-02-08 09:24:46 +0100

    .

---
+++ SoftCard/softCard/Default/TestServer.java
@@ -0,0 +1,17 @@
+package Default;
+
+/**
+ * This class allows us to test
+ * the {@link SoftCard} class.
+ * @author Emmanuel Mocquet
+ * @version 1.0 
+ */
+public class TestServer {
+
+	public static void main(String[] args) {
+		
+		new SoftCard("192.168.34.10", 42425, 15);
+		
+	}
+	
+}
