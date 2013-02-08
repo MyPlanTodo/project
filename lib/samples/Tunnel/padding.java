fc907bc093327a7bb57feeedd7b59749de0429e2
romain_pignard <romain.pignard@gmx.com>
2013-02-08 10:21:16 +0200

    ajout de code

---
+++ lib/samples/Tunnel/padding.java
@@ -0,0 +1,121 @@
+package store;
+
+import javacard.framework.APDU;
+
+import javacard.framework.Applet;
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.JCSystem;
+import javacardx.crypto.*;
+
+public class padding extends Applet {
+	private static byte[] padded;	
+	private static short[] tab;
+	
+	public static final byte CLA_MONAPPLET = (byte) 0xB0;
+	
+	public static final byte INS_PAD = 0x00;
+	public static final byte INS_UNPAD = 0x01;
+	//private Cipher chiff;
+	
+
+	private padding() {
+		// padded contains is the temporary storage for the (un)padded message
+		padded = JCSystem.makeTransientByteArray((short) 96, JCSystem.CLEAR_ON_RESET);
+		
+		// tab is a temporary short array for loop variables and array length
+		tab = JCSystem.makeTransientShortArray((short) 96, JCSystem.CLEAR_ON_RESET);
+	}
+
+	public static void install(byte bArray[], short bOffset, byte bLength)
+			throws ISOException {
+		new padding().register();
+	}
+
+	public static short pad(byte[] sortie, byte[] entree, short blockSize, short lg, byte bufOff)
+	{
+					
+		// copy of the original message into padded 
+		for(tab[0] = 0; tab[0] < (short) (lg); tab[0]++)
+		{
+			padded[tab[0]] = entree[(short)(tab[0]+bufOff)];
+		}
+		// padding of the message according to pkcs7		
+		if(lg % blockSize == 0)
+		{	
+			// if the last block is full, we create another full block 
+			for( tab[0] = (short) lg; tab[0] < (short) (lg +  blockSize) ; tab[0]++)
+			{
+				
+				padded[tab[0]] = (byte)  blockSize;
+			}
+		}
+		else
+		{
+			// we fill the last block with the required number of bytes
+			for(tab[0] =(short) lg; tab[0] < (short) (lg + blockSize -  (lg % blockSize)); tab[0]++)
+			{
+				padded[tab[0]] = (byte) (blockSize - lg  % blockSize);
+			}
+		}
+		tab[1] = (short) (lg + blockSize -  (lg % blockSize));
+		//tab[1] = (short)( lg - entree[(short)(lg - 1 + bufOff)]);
+		for( tab[0] = 0;tab[0]<tab[1];tab[0]++)
+		{
+			sortie[tab[0]] = padded[tab[0]]; 
+			
+		}
+		
+		
+		// length of the padded message
+		return tab[1];
+	}	
+	
+	public static short unpad(byte[] buffIn,byte[] mess, short blockSize, short lg, short bufOff)
+	{
+		
+		// removing of the padding
+		for(tab[(short)0] = 0; tab[(short)0] <(short)( lg - mess[(short)(lg-1 +bufOff) ]);tab[(short)0]++ )
+		{
+			padded[tab[0]] = mess[(short)(tab[0] + bufOff)];
+			
+		}
+		tab[1] = (short)( lg - mess[(short)(lg - 1 + bufOff)]);
+		for( tab[0] = 0;tab[0]<tab[1];tab[0]++)
+		{
+			buffIn[tab[0]] = padded[tab[0]]; 
+			
+		}
+		
+		// length of the unpadded message
+		return tab[1];				
+	}
+	
+	
+	
+	public void process(APDU apdu) throws ISOException {
+		byte[] buffer = apdu.getBuffer();
+		
+		
+		if (this.selectingApplet()) return;
+		
+		if (buffer[ISO7816.OFFSET_CLA] != CLA_MONAPPLET) {
+			ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+		}
+		
+		switch (buffer[ISO7816.OFFSET_INS]) {
+			case INS_UNPAD:			
+				tab[1] =   unpad(buffer,buffer,buffer[ISO7816.OFFSET_P2],buffer[ISO7816.OFFSET_P1],ISO7816.OFFSET_CDATA);				
+				break;
+			case INS_PAD:
+				tab[1] =  pad(buffer,buffer,buffer[ISO7816.OFFSET_P2],buffer[ISO7816.OFFSET_P1],ISO7816.OFFSET_CDATA);
+				break;
+			default:
+				ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+		}
+						
+		apdu.setOutgoingAndSend((short) 0, tab[1]);
+
+	}
+
+}
---
+++ lib/samples/nombres_aleatoires/client_gen.java
@@ -0,0 +1,103 @@
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.smartcardio.Card;
+import javax.smartcardio.CardChannel;
+import javax.smartcardio.CardException;
+import javax.smartcardio.CardTerminal;
+import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
+import javax.smartcardio.TerminalFactory;
+
+public class client_gen {
+	/* Constantes */
+	public static final byte CLA_MONAPPLET = (byte) 0xB0;
+
+		
+	public static byte[] APPLET_AID= { (byte)0x01, (byte)0x02, (byte)0x03, (byte)0x04, (byte)0x05, (byte)0x06, (byte)0x07, (byte)0x08, (byte)0x09, (byte)0x00, (byte)0x00 };
+	
+	public static void main(String[] args) throws IOException {
+	        ResponseAPDU r;
+
+		/* Connexion au lecteur */
+		TerminalFactory factory = TerminalFactory.getDefault();
+		List<CardTerminal> terminals;
+		try {
+			terminals = factory.terminals().list();
+
+			System.out.println("Terminaux : "+terminals);
+
+			CardTerminal terminal = terminals.get(0);
+
+			/* Connexion à la carte */
+			Card card = terminal.connect("T=1");
+			System.out.println("Carte : "+card);
+
+			CardChannel channel = card.getBasicChannel();
+
+			/* Sélection de l'applet */
+			r = channel.transmit(new CommandAPDU((byte)0x00, (byte)0xA4, (byte)0x04, (byte)0x00, APPLET_AID));
+			if (r.getSW() != 0x9000) {
+				System.out.println("Erreur lors de la sélection de l'applet");
+				System.exit(1);
+			}
+
+			/* Menu principal */
+			boolean fin = false;
+			while (!fin) {
+				System.out.println();
+				System.out.println("Application cliente Javacard");
+				System.out.println("----------------------------");	
+				System.out.println("Votre choix ?");
+
+				
+			
+				
+				 Scanner in = new Scanner(System.in); 
+			       System.out.printf("Enter i Value:  ");
+			       int choix = in.nextInt();
+			
+			
+
+			      // FileWriter fstream = new FileWriter("out.txt");
+					//	  BufferedWriter out = new BufferedWriter(fstream);
+			     
+			     
+			    	  
+				//int choix = 10;
+				//System.out.println("Valeur du compteur : "+choix);
+					r = channel.transmit(new CommandAPDU((byte)0xB0, 0x00, (byte) choix, (byte)0x00));
+
+					if (r.getSW() != 0x9000) {
+						System.out.println("Erreur : status word different de 0x9000 : "+r.getSW());
+					} else {
+						//System.out.println("Valeur du compteur : " );
+						for(int i= 0; i < choix;i++)
+						{	
+						System.out.println(i+" = "+ r.getData()[i]);
+						}
+					}
+					for(int i= 0; i < choix;i++){
+					
+					//  out.write(r.getData()[i]);
+					  //Close the output stream
+					}  
+					 
+				
+			       
+
+			}
+
+			/* Mise hors tension de la carte */
+			card.disconnect(false);
+
+		} catch (CardException e) {
+			e.printStackTrace();
+		}
+	}
+}
